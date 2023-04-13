bcd2801e811cec80752518c6c2d1f56476a9c81b
hetpatel66 <22847827+hetpatel66@users.noreply.github.com>
2023-04-13 17:59:19 -0400

    Add code to change scenes.

--- src/main/java/com/example/montyhall/SceneController.java
+++ src/main/java/com/example/montyhall/SceneController.java
@@ -1,14 +1,33 @@
 package com.example.montyhall;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Node;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.stage.Stage;
+
+import java.io.IOException;
 
 public class SceneController {
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
     @FXML
     private Label welcomeText;
 
     @FXML
     protected void onHelloButtonClick() {
         welcomeText.setText("Welcome to JavaFX Application!");
+    }
+
+    public void switchToGameScene(ActionEvent event) throws IOException {
+        root = FXMLLoader.load(Game.class.getResource("game.fxml"));
+        stage = (Stage)((Node)event.getSource()).getScene().getWindow();
+        scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 }
