10ac2444a941f909d549308ff693deffde5e2d35
hetpatel66 <22847827+hetpatel66@users.noreply.github.com>
2023-04-20 18:58:25 -0400

    Remove unnecessary imports from Game and SceneController.

--- src/main/java/com/example/montyhall/Game.java
+++ src/main/java/com/example/montyhall/Game.java
@@ -4,17 +4,8 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Font;
-import javafx.scene.text.TextAlignment;
 import javafx.stage.Stage;
 
 import java.io.IOException;
--- src/main/java/com/example/montyhall/SceneController.java
+++ src/main/java/com/example/montyhall/SceneController.java
@@ -6,7 +6,6 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Node;
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.stage.Stage;
